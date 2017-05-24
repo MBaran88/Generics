86d569aa91054222d296326a5b1b827cf3b09b15
MBaran88 <zdzibq@gmail.com>
2017-05-24 20:18:33 +0200

    commit #1

---
+++ src/com/MyArrayList/ArrayList.java
@@ -0,0 +1,21 @@
+package com.MyArrayList;
+
+/**
+ * Created by RENT on 2017-05-24.
+ */
+public class ArrayList {
+    private Object data;
+
+    public void add(Object o) {
+        this.data = data;
+
+    }
+    public int getSize() {
+        ///
+        return 0;
+    }
+    public Object get(int Index){
+        ///
+        return null;
+    }
+}
---
+++ src/com/MyArrayList/Main.java
@@ -0,0 +1,8 @@
+package com.MyArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
